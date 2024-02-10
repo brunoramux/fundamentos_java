f84b2d674c89c14ecd13d33e84ea4e8b58216af0
brunoramux <bruno.lemos@live.com>
2024-02-10 17:58:00 -0300

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String args[]) {
+        int teste = 10;
+
+        if (teste == 11){
+            System.out.println(teste);
+        } else {
+            System.out.println("Falhou");
+        }
+
+        List<String> nomes  = new ArrayList<>();
+        nomes.add("Bruno");
+        nomes.add("Thamires");
+
+        System.out.println(nomes);
+        System.out.println(nomes.get(0));
+        
+        for(String nome : nomes){
+            System.out.println(nome);
+        }
+
+        nomes.forEach(nome -> System.out.println(nome));
 
     }
 }
