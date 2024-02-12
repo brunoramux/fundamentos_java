22b952a27e53e5e7eaff3fb892d129e830887f05
brunoramux <bruno.lemos@live.com>
2024-02-12 01:16:29 -0300

    FINAL COMMIT

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HelloWorld {
     public static void main(String args[]) {
@@ -24,5 +28,26 @@ public class HelloWorld {
 
         nomes.forEach(nome -> System.out.println(nome));
 
+        Map<String, Integer> notas = new HashMap<>();
+        notas.put("Bruno", 10);
+        notas.get("Bruno"); // retorna 10
+
+        //iterando
+
+        for(Map.Entry<String, Integer> entry : notas.entrySet()){
+            // tenho acesso a cada um dos itens do HashMap
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+        }
+
+        Set<Integer> numeros = new HashSet<>();
+
+        numeros.add(10);
+        numeros.remove(10);
+
+        for(Integer elemento : numeros) {
+
+        }
+        numeros.contains(10); // retorna boleano se existe ou não o item
     }
 }
